f61958928906b096f75c500b5a23723d99610d6b
mrendy58 <derendyyy58@gmail.com>
2024-11-21 17:12:47 +0700

    membuat enum class halaman untuk mendefinisukan 2 halaman

---
+++ app/src/main/java/com/example/p5arsitekturmvvm/Navigationcontrol.kt
@@ -0,0 +1,23 @@
+package com.example.p5arsitekturmvvm
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.p5arsitekturmvvm.model.JenisKelamin.jeniskelamin
+import com.example.p5arsitekturmvvm.ui.view.FormulirView
+import com.example.p5arsitekturmvvm.ui.view.TampilDataView
+import com.example.p5arsitekturmvvm.viewmodel.SiswaViewModel
+
+enum class Halaman {
+    FORMULIR,
+    TAMPILDATA
+}
+
+
