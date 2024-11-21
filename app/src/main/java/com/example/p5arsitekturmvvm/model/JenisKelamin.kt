30be4585f15795e49e9b2c8cf2961882804e5c3f
mrendy58 <derendyyy58@gmail.com>
2024-11-21 17:52:49 +0700

    membuat objek gender untuk option di radio button

---
+++ app/src/main/java/com/example/p5arsitekturmvvm/model/JenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.p5arsitekturmvvm.model
+
+import com.example.p5arsitekturmvvm.R
+
+object JenisKelamin {
+    val jeniskelamin = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
