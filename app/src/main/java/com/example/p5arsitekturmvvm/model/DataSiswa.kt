aa7e844891ae3df5ff494b4d1cb34afa88626d96
mrendy58 <derendyyy58@gmail.com>
2024-11-21 17:55:08 +0700

    model yang digunakan untuk menyimpan data di aplikasi

---
+++ app/src/main/java/com/example/p5arsitekturmvvm/model/DataSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.p5arsitekturmvvm.model
+
+data class DataSiswa(
+    val nama: String = "",
+    val alamat: String = "",
+    val nim: String = "",
+    val gender: String = "",
+    val email: String = "",
+    val notelpon : String = "",
+)
